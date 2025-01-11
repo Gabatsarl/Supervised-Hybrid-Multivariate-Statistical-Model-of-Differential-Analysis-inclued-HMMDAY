87766e62c8a8286a993cded1e476bdb5e18345a0
@Gabatsarl <77648101+Gabatsarl@users.noreply.github.com>
2025-01-11 13:02:56 -0500

    Update descriptive.R

--- descriptive.R
+++ descriptive.R
@@ -32,16 +32,14 @@ stopCluster(cl)
 
 
 
-##reunir les resultats------------
+##--------Reunir les resultats------------##
 
 maxim<-vector(mode = "numeric", length = n) ; media<-vector(mode = "numeric", length = n) ; sde<-vector(mode = "numeric", length = n)
 
 for(i in 1:n){
-  maxim[i]<-DES_DATA[[i]][[1]] ;
-  media[i]<-DES_DATA[[i]][[2]] ;
-  sde[i]<-DES_DATA[[i]][[3]]
- 
-
+              maxim[i]<-DES_DATA[[i]][[1]] ;
+              media[i]<-DES_DATA[[i]][[2]] ;
+              sde[i]<-DES_DATA[[i]][[3]]
    }
 
 
