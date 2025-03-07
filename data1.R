b5be6134821e4843c24df4564bb19eb10c2029b6
@Gabatsarl <77648101+Gabatsarl@users.noreply.github.com>
2025-03-07 10:34:05 -0800

    Update data1.R

--- data1.R
+++ data1.R
@@ -20,9 +20,9 @@ mapping_TRAIN <- read.csv("/mapping_TRAIN.tsv", sep= ";", comment.char = "*", he
 ### ---------------- Correction d'une variables sous ------------ ##
 
 groups<-NA
-groups[which((mapping_TRAIN$Sample_type=="No_plant")&(mapping_TRAIN$Water_type== "OSPW"))]<-"OSPW"
-groups[which(mapping_TRAIN$Sample_type=="Carex")] <- "OSPW+Carex"
-groups[which((mapping_TRAIN$Sample_type=="No_plant")&(mapping_TRAIN$Water_type=="Artificial_OSPW"))] <- "Control"
+groups[which((mapping_TRAIN$Sample_type== "No_plant" )&(mapping_TRAIN$Water_type== "OSPW"))]<-"OSPW"
+groups[which(mapping_TRAIN$Sample_type== "Carex")] <- "OSPW+Carex"
+groups[which((mapping_TRAIN$Sample_type== "No_plant")&(mapping_TRAIN$Water_type== "Artificial_OSPW"))] <- "Control"
 mapping_TRAIN$groups<-groups
 
 ####----------supprimer les colonnes non moins importantes -------##
